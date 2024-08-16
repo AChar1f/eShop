1381d9217036ebd3377b6b8f7d0905d8080f6022
AChar1f <acharif41@gmail.com>
2024-08-16 14:43:56 +0200

    card slot added

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+            <slot name="cardHeader"></slot>
+        </div>
+        <div class="card-body">
+            <slot name="cardBody"></slot>
+  </div>
+</div>
+</template>
+<script>
+export default {
+    name: "CardComp",
+    components: {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
