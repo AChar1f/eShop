c7ad76a34317f749512dc2cf446b6d6170926c83
AChar1f <acharif41@gmail.com>
2024-08-17 09:05:01 +0200

    navbar and footer components

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <p class="lead">AC's Store &copy; <span id="currYear">{{ currYear }}</span></p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "FooterComp",
+    data() {
+    return {
+      currYear: new Date().getUTCFullYear(),
+    };
+  }
+}
+</script>
+<style scoped>
+    
+</style>
