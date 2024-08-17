4ca09c09c97123a365b9e24cc8efb8199a55225e
AChar1f <acharif41@gmail.com>
2024-08-17 14:52:10 +0200

    updated addProd emsg

--- model/Products.js
+++ model/Products.js
@@ -82,7 +82,7 @@ class Products {
         } catch (e) {
             res.json({
                 status: 404,
-                msg: e.message
+                err: e.message
             })
         }
     }
